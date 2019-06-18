895e51dd78c670c1c96f19dea348bfa707463c53
Mark Ahrens <ahrens.mark.a@gmail.com>
2019-06-18 15:40:28 -0500

    while loops exercise corrected for undefined and linked correctly

--- js/while.js
+++ js/while.js
@@ -1,8 +1,33 @@
 "use strict";
 
-var i = 0;
+// While loops #2
+var x = 0;
+var y = 0;
+//var y = math.pow(2,x + 2);
 
-while (i < 10) {
-    console.log('while loop iteration #' + i);
-    i++;
+while (x < 65536) {
+    x = Math.pow(2, y+1);
+    y++;
+    console.log(x);
+
 }
+
+// While loops #3
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+var soldToCustomer = 0;
+var totalSold = 0
+
+
+do {
+    soldToCustomer = Math.floor(Math.random() * 5) + 1;
+    console.log(soldToCustomer + " Cones sold");
+    totalSold = soldToCustomer + totalSold;
+
+} while (totalSold < allCones);
+if (totalSold >= allCones) {
+    var cannotSell = totalSold - allCones;
+}
+console.log("Cannot sell you " + soldToCustomer + " cones, I only have " + cannotSell + " left.");
+console.log(allCones + " is how many cones I had to sell");
+
