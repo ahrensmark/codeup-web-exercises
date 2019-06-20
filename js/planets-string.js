be7e462a100b43bf78daafc9f6c1aa720c7a9543
Mark Ahrens <ahrens.mark.a@gmail.com>
2019-06-20 11:42:04 -0500

    split-join exercise

--- js/planets-string.js
+++ js/planets-string.js
@@ -26,6 +26,7 @@
      // * list. You will need an opening AND closing <ul> tags around the entire
      // * string, and <li> tags around each planet.
      // *
-    
-
+    // var planetsUl = "<ul><li>" + planetsArray.join("</li></ul>") + "<ul><li>";
+    //
+    //     document.getElementById("good-time").innerTML = planetsUL;
 })();
